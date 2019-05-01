d3c364e21aeece07d7c7a1906f1e78454df78207
Technote <technote.space@gmail.com>
2019-05-02 01:35:08 +0900

    trivial change

--- PHP/main.php
+++ PHP/main.php
@@ -1,2 +1,1 @@
-<?php
-foreach (range(1, 100) as $i) echo (0 === $i % 15 ? 'FizzBuzz' : (0 === $i % 3 ? 'Fizz' : (0 === $i % 5 ? 'Buzz' : $i))) . "\n";
+<?php　foreach (range(1, 100) as $i) echo (0 === $i % 15 ? 'FizzBuzz' : (0 === $i % 3 ? 'Fizz' : (0 === $i % 5 ? 'Buzz' : $i))) . "\n";
